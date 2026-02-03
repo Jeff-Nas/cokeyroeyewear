e35b5779ebb110958a2b84268bc0e7e13726a929
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-03 18:21:36 -0300

    Criando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import { Header } from "./Header";
-import { Products } from "./Products";
 import { ProductsProvider } from "./ProductsProvider";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
+import Cart from "./";
 
 function App() {
   return (
     <ProductsProvider>
       <div>
         <Header />
-        <Products />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
       </div>
     </ProductsProvider>
   );
