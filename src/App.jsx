54532f8ed3f0717e5d0e4e985ff07916c6ce1614
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-06 17:24:03 -0300

    Adicionando ID na rota product

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <ProductSlider />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
