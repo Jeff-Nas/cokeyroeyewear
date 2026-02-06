eeeed7ecf80bb38c5b8b2c3ed92915cc7981a20d
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-06 17:24:35 -0300

    Fazendo setup da página Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,1 +1,19 @@
-export default function Product() {}
+import { ProductSlider } from "../ProductSlider";
+import { useProducts } from "../ProductsProvider";
+import { useParams } from "react-router-dom";
+
+export default function Product() {
+  const { id } = useParams();
+  const { displayed } = useProducts();
+  const product = displayed.find((item) => item.id === Number(id)); //o router entrega id em string
+  const gallery = product.images.gallery;
+  console.log(gallery);
+
+  return (
+    <main>
+      <div>
+        <h2>{product.name}</h2>
+      </div>
+    </main>
+  );
+}
