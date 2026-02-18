0b281675368814970f94b4cfefb0adae20bc0f71
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-02-18 19:10:08 -0300

    Ajustes para telas desktop

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -21,7 +21,7 @@ export default function Product() {
           <ProductSlider gallery={gallery} />
         </div>
         {/* CARD DESCRIÇÃO DO PRODUTO */}
-        <div className="flex flex-col">
+        <div className="flex flex-col lg:w-112.5">
           <div className="ml-4 mr-4 flex flex-col justify-end h-full lg:mb-6">
             <h2 className="text-2xl md:text-4xl lg:text-5xl font-cairo-style mb-3">
               {product.name}
@@ -35,7 +35,7 @@ export default function Product() {
                 {formatCurrency(Math.floor(discount.discountAmount))}
               </p>
               {/* CALL TO ACTION BUTTONS */}
-              <div className="flex gap-2 mt-4 mb-4 max-w-96 lg:max-w-125 ">
+              <div className="flex gap-2 mt-4 mb-4 max-w-96 lg:max-w-none lg:w-full">
                 <button className="font-display flex-1 text-white bg-gray-900 px-4 py-2.5 rounded">
                   <div className="flex justify-between items-center lg:p-1.5">
                     <span className="lg:text-xl">PEDIR NO WHATSAPP</span>
